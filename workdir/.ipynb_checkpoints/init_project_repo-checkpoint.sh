871bbd015aa279a7a0808584da92e2c0cb5bf05d
Yuhui Shi <yuhuishi@umich.edu>
2020-09-06 11:18:32 -0700

    commit everything test

---
+++ workdir/.ipynb_checkpoints/init_project_repo-checkpoint.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+
+# first time initialization
+
+
+# create the github remote repo if not initialized
+
+# initialize the local git repo
+
+# add remote origin to the local repo
+
+# add default gitignore file
+
+# sync part 
+
+# git add . && git commit -m 'auto commit' && git push origin master
